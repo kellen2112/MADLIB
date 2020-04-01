118a72e8f6a6a58f9d55fed05004b767785585cd
Kellen Sims <kellen2112@gmail.com>
2020-03-31 20:50:05 -0600

    added full random functions, cleaned up and linked arrays and js to html, cleaned up some errors

---
+++ defaultWords.js
@@ -0,0 +1,23 @@
+var defaultWords = {
+    noun: [
+        'father','teenager','lion','cat','pug','book','table','coat','wolf','minister',
+        'teacher','child','woman','man','alligator','tiger','bird','zoo','city','state','bear',
+    ],
+
+    verb: [
+        'bend','beat','bite','blow','burn','cut','dig','drive','feel',
+        'fight','fly','forgive','hang','hear','hurt','lay','lead','leave','make','pay',
+        'send','shut','sing','teach','throw','understand','wake','wear','win','write', 
+    ],
+
+    adverb: [
+        'awkwardly','brutally','absentmindedly','carefully','eagerly','gracefully','lazily',
+        'quickly','recklessly','savagely','urgently','extremely','rather','very',
+        'terribly','really','abroad','down','everywhere','inside','outside','somewhere','there','underground',
+    ],
+
+    adjective: [
+        'gigantic','colossal','immense','mammoth','scrawny','pitiful','uptight','flabby','puny','teeny','miniature',
+        'bewildered','clumsy','grumpy','itchy','repulsive','thoughtless','scary','nervous','fierce','mysterious',
+    ]
+}
